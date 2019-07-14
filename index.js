4b925f8a55142cbb763baa554496612f4b9fbfa7
Livitan <>
2019-07-14 12:43:52 +0300

    add fetch

--- index.js
+++ index.js
@@ -1,13 +1,33 @@
+'use strict';
+
 const Discord = require("discord.js");
+const fetch = require('node-fetch');
+
 const client = new Discord.Client();
 
-client.on("ready", () => {
-	console.log("I am ready!");
+let currencyResult;
+
+client.on('ready', () => {
+	console.log("Connected as " + client.user.tag);
+	client.user.setActivity("JavaScript");
 });
+
+fetch("https://api.privatbank.ua/p24api/pubinfo?json&exchange&coursid=5")
+	.then(function (response) {
+		return response.json();
+	})
+	.then(function (data) {
+		console.log(data);
+		let usdCcy = data[0];
+		let rubCcy = data[2];
+		let usdArr = usdCcy.ccy + ' ' + usdCcy.buy;
+		let rubArr = rubCcy.ccy + ' ' + rubCcy.buy;
+		currencyResult = ':thinking:' + '\n' + ':flag_us:' + ' ' + usdArr + '\n' + ':flag_ru:' + ' ' + rubArr;
+	});
 
 client.on("message", (message) => {
 	if (message.content.startsWith("ping")) {
-		message.channel.send("pong!");
+		message.channel.send(currencyResult);
 	}
 });
 
